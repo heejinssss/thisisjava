8ec9dd9e2dbc130723d9777f778a8432e2ab003c
heejinssss <heejin.dev33@gmail.com>
2024-03-19 16:39:36 +0900

    Feat: Ch02.Sec01

--- src/ch02/sec01/VariableInitializationExample.java
+++ src/ch02/sec01/VariableInitializationExample.java
@@ -7,6 +7,11 @@ public class VariableInitializationExample {
 		int age; // 변수 선언
 		age = 15; // 변수에 초기값 15 저장
 		System.out.println(age);
+		
+		/*
+		int value;
+		int result = value + 10; // 컴파일 에러 발생		
+		*/
 
 	}
 
