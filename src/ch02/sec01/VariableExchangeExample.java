c6dcf52a095a8d0f920ad6bb565b46966f290141
heejinssss <heejin.dev33@gmail.com>
2024-03-19 17:05:58 +0900

    Feat: Ch02.Sec01

---
+++ src/ch02/sec01/VariableExchangeExample.java
@@ -0,0 +1,17 @@
+package ch02.sec01;
+
+public class VariableExchangeExample {
+
+	public static void main(String[] args) {
+
+		int x = 3;
+		int y = 5;
+		
+		int tmp = x;
+		x = y;
+		y = tmp;
+		System.out.println("x:" + x + ", y:" + y);
+
+	}
+
+}
