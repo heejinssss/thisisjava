77f4f79a51ff0dc5f27f47d3f13d9509ff35e539
heejinssss <heejin.dev33@gmail.com>
2024-03-20 15:18:38 +0900

    Chore: Ch02.Sec04

--- src/ch02/sec04/FloatDoubleExample.java
+++ src/ch02/sec04/FloatDoubleExample.java
@@ -1,9 +1,7 @@
 package ch02.sec04;
 
 public class FloatDoubleExample {
-
 	public static void main(String[] args) {
-
 		// 정밀도 검증
 		float var1 = 0.1234567890123456789f;
 		double var2 = 0.1234567890123456789;
