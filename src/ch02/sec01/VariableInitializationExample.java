9341e7084b0d6fc33de695a0cbc3e9d3a2e37cbf
heejinssss <heejin.dev33@gmail.com>
2024-03-20 15:18:14 +0900

    Chore: Ch02.Sec01

--- src/ch02/sec01/VariableInitializationExample.java
+++ src/ch02/sec01/VariableInitializationExample.java
@@ -1,17 +1,13 @@
 package ch02.sec01;
 
 public class VariableInitializationExample {
-
 	public static void main(String[] args) {
-
 		int age; // 변수 선언
 		age = 15; // 변수에 초기값 15 저장
 		System.out.println(age);
 		
-		/*
-		int value;
-		int result = value + 10; // 컴파일 에러 발생		
-		*/
+//		int value;
+//		int result = value + 10; // 컴파일 에러 발생		
 
 	}
 
