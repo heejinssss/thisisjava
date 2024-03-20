af13b1b5b40e2a213641be4ba68a384044d9be37
heejinssss <heejin.dev33@gmail.com>
2024-03-20 15:20:23 +0900

    Chore: Ch02.Sec08

--- src/ch02/sec08/CastingExample.java
+++ src/ch02/sec08/CastingExample.java
@@ -1,9 +1,7 @@
 package ch02.sec08;
 
 public class CastingExample {
-
 	public static void main(String[] args) {
-
 		int var1 = 10;
 		byte var2 = (byte) var1;
 		System.out.println(var2); // 강제 타입 변환 후에 10이 그대로 유지
