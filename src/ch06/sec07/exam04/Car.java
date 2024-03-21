e5c3a9ba8583a1c70c3e1ad8f6de872b50d54943
heejinssss <heejin.dev33@gmail.com>
2024-03-21 15:51:48 +0900

    Feat: Ch06.Sec07.Exam04

--- src/ch06/sec07/exam04/Car.java
+++ src/ch06/sec07/exam04/Car.java
@@ -9,20 +9,19 @@ public class Car {
 
 	Car() {
 	}
-	
+
 	Car(String model) {
 		this.model = model;
 	}
-	
+
 	Car(String model, String color) {
 		this.model = model;
 		this.color = color;
 	}
-	
+
 	Car(String model, String color, int maxSpeed) {
 		this.model = model;
 		this.color = color;
 		this.maxSpeed = maxSpeed;
 	}
-
 }
