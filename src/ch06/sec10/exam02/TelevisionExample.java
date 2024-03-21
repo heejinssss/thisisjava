552131aa950ff69b052a070da9d9116dbaeb5006
heejinssss <heejin.dev33@gmail.com>
2024-03-21 14:34:31 +0900

    Feat: Ch06.Sec10.Exam02

---
+++ src/ch06/sec10/exam02/TelevisionExample.java
@@ -0,0 +1,10 @@
+package ch06.sec10.exam02;
+
+public class TelevisionExample {
+	public static void main(String[] args) {
+//		Television tv = new Television();
+		System.out.println(Television.info);
+
+	}
+
+}
