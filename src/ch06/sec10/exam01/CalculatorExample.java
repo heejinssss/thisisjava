b544716314c52568447943d334937ae4a8bc87a0
heejinssss <heejin.dev33@gmail.com>
2024-03-21 15:54:00 +0900

    Feat: Ch06.Sec10.Exam01

--- src/ch06/sec10/exam01/CalculatorExample.java
+++ src/ch06/sec10/exam01/CalculatorExample.java
@@ -5,10 +5,8 @@ public class CalculatorExample {
 		double result1 = 10 * 10 * Calculator.pi;
 		int result2 = Calculator.plus(10, 5);
 		int result3 = Calculator.minus(10, 5);
-		
+
 		System.out.println("result1: " + result1);
 		System.out.println("result2: " + result2);
-
 	}
-
 }
