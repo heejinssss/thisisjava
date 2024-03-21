acfcdf12c932777d6be5e99920606d8feec6c159
heejinssss <heejin.dev33@gmail.com>
2024-03-21 15:44:31 +0900

    Feat: Ch03.Sec01

--- src/ch03/sec01/IncreaseDecreaseOperatorExample.java
+++ src/ch03/sec01/IncreaseDecreaseOperatorExample.java
@@ -5,32 +5,30 @@ public class IncreaseDecreaseOperatorExample {
 		int x = 10;
 		int y = 10;
 		int z;
-		
+
 		x++; // 11
 		y++; // 11
 		System.out.println("x=" + x); // 11
-		
+
 		System.out.println("=============");
 		y--; // 11
 		--y; // 9
 		System.out.println("y=" + y); // 9
-		
+
 		System.out.println("=============");
 		z = x++; // 11
 		System.out.println("z=" + z); // 11
 		System.out.println("x=" + x); // 12
-		
+
 		System.out.println("=============");
 		z = ++x; // 13
 		System.out.println("z=" + z); // 13
 		System.out.println("x=" + x); // 13
-		
+
 		System.out.println("=============");
 		z = ++x + y++; // 14 + 9
 		System.out.println("z=" + z); // 23
 		System.out.println("x=" + x); // 14
 		System.out.println("y=" + y); // 10
-
 	}
-
 }
