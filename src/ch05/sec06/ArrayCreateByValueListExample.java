0a0858fdcd6b23a94732533e7495bd048ec97557
heejinssss <heejin.dev33@gmail.com>
2024-03-21 15:48:40 +0900

    Feat: Ch05.Sec06

--- src/ch05/sec06/ArrayCreateByValueListExample.java
+++ src/ch05/sec06/ArrayCreateByValueListExample.java
@@ -12,11 +12,11 @@ public class ArrayCreateByValueListExample {
 			sum1 += scores[i];
 		}
 		System.out.println("총합: " + sum1);
-		
+
 		// 배열을 매개값으로 주고, printItem() 메소드 호출
 		printItem( new int[] { 83, 90, 87 });
 	}
-	
+
 	// printItem() 메소드 선언
 	public static void printItem( int[] scores ) {
 		// 매개변수가 참조하는 배열의 항목을 출력
@@ -24,5 +24,4 @@ public class ArrayCreateByValueListExample {
 			System.out.println("score[" + i + "]: " + scores[i]);
 		}
 	}
-
 }
