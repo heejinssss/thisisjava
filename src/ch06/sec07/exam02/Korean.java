3ee0425b86ec492bd527e52ea9999ef28da985bd
heejinssss <heejin.dev33@gmail.com>
2024-03-21 15:50:57 +0900

    Feat: Ch06.Sec07.Exam02

--- src/ch06/sec07/exam02/Korean.java
+++ src/ch06/sec07/exam02/Korean.java
@@ -5,11 +5,10 @@ public class Korean {
 	String nation = "대한민국";
 	String name;
 	String ssn;
-	
+
 	// 생성자 선언
 	public Korean(String n, String s) {
 		name = n;
 		ssn = s;
 	}
-
 }
--- src/ch06/sec07/exam02/KoreanExample.java
+++ src/ch06/sec07/exam02/KoreanExample.java
@@ -9,14 +9,12 @@ public class KoreanExample {
 		System.out.println("k1.name: " + k1.name);
 		System.out.println("k1.ssn: " + k1.ssn);
 		System.out.println();
-		
+
 		// 또 다른 Korean 객체 생성
 		Korean k2 = new Korean("사스케", "012606");
 		// 또 다른 Korean 객체 데이터 읽기
 		System.out.println("k2.nation: " + k2.nation);
 		System.out.println("k2.name: " + k2.name);
 		System.out.println("k2.ssn: " + k2.ssn);
-
 	}
-
 }
