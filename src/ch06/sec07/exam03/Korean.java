79c8e3476314a41551be4642f6df79d502bd4b52
heejinssss <heejin.dev33@gmail.com>
2024-03-21 15:51:17 +0900

    Feat: Ch06.Sec07.Exam03

--- src/ch06/sec07/exam03/Korean.java
+++ src/ch06/sec07/exam03/Korean.java
@@ -5,11 +5,10 @@ public class Korean {
 	String nation = "대한민국";
 	String name;
 	String ssn;
-	
+
 	// 생성자 선언
 	public Korean(String name, String ssn) {
 		this.name = name;
 		this.ssn = ssn;
 	}
-	
 }
