ed2af1494e53ab03b5a53a10d5453a7541180b96
heejinssss <heejin.dev33@gmail.com>
2024-03-21 15:53:35 +0900

    Feat: Ch06.Sec08.Exam03

--- src/ch06/sec08/exam03/Car.java
+++ src/ch06/sec08/exam03/Car.java
@@ -3,12 +3,12 @@ package ch06.sec08.exam03;
 public class Car {
 	// 필드 선언
 	int gas;
-	
+
 	// 리턴값이 없는 메소드로 매개값을 받아서 gas 필드값을 변경
 	void setGas(int gas) {
 		this.gas = gas;
 	}
-	
+
 	// 리턴값이 boolean인 메소드로 gas 필드값이 0이면 false를, 0이 아니면 true를 리턴
 	boolean isLeftGas() {
 		if(gas == 0) {
@@ -18,7 +18,7 @@ public class Car {
 		System.out.println("가스가 있습니다.");
 		return true; // return을 리턴하고 메소드 종료
 	}
-	
+
 	// 리턴값이 없는 메소드로 gas 필드값이 0이면 return 문으로 메소드를 종료
 	void run() {
 		while (true) {
@@ -31,5 +31,4 @@ public class Car {
 			}	
 		}
 	}
-
 }
